0ba4aa5ad1b1ca3c2990d0bf1e77376aec74a7a2
Binayak Dhakal <you@example.com>
2019-04-15 23:56:17 +0545

    changes

--- app/views/students/studentHome.php
+++ app/views/students/studentHome.php
@@ -1,7 +1,7 @@
 
 <div class = "boxesContainer">
   <div class = "contentBoxLarge tutorialVideo">
-    <div class = "title" style="background: olive;">Tutorial</div>
+    <div class = "title">Tutorial</div>
     <div class = "content" style="margin:0; max-height: 295px;">
       <video src = "resources/videos/tutorial.mp4#t=07,20" width="100%" height="295px" controls>Video Not Found</video>
     </div>
@@ -9,7 +9,7 @@
 
 
   <div class = "contentBoxLarge recentAnnouncements">
-    <div class = "title" style="background: red;">Recent Announcements</div>
+    <div class = "title">Recent Announcements</div>
     <div class = "content" style="margin:0; overflow-Y: auto; max-height: 295px; text-align: left;" id="customScroll">
       <?php
         while($announcement = $announcements->fetch()){
